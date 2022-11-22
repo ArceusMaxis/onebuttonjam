5ef4795e73b11b804787be39cf571f213ed0f2e6
Amirtha Krishnan <76719897+ArceusMaxis@users.noreply.github.com>
2022-11-22 16:24:34 +0530

    Add files via upload

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed = rand_range(300,900)
+var speed = rand_range(500,900)
 var velocity = Vector2.ZERO
 onready var target = get_node("../Player")
 var deadfx = preload("res://scenes/Deathfx.tscn")
@@ -10,15 +10,16 @@ func _physics_process(delta):
 	look_at(target.position)
 	velocity = position.direction_to(target.position) * speed
 	velocity = move_and_slide(velocity)
-	
 
 func _on_screen_exited():
 	queue_free()
 
-
-func _on_Enemybox_area_entered(area):
+func _on_area_entered(area):
 	if area.is_in_group("shield"):
 		queue_free()
 		var dinst = deadfx.instance()
 		get_parent().add_child(dinst)
 		dinst.global_position = global_position
+		
+	if area.is_in_group("player"):
+		get_tree().change_scene("res://scenes/Rip.tscn")
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -15,4 +15,4 @@ func _physics_process(delta):
 
 func _on_Hitbox_entered(body):
 	if body.is_in_group("enemy"):
-		get_tree().quit()
+		get_tree().change_scene("res://scenes/Rip.tscn")
