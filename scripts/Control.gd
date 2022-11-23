c976263290789140cbb675839cec670ae05acc81
Amirtha Krishnan <76719897+ArceusMaxis@users.noreply.github.com>
2022-11-23 16:27:07 +0530

    Add files via upload

--- scripts/Control.gd
+++ scripts/Control.gd
@@ -1,8 +1,4 @@
 extends Control
 
-func _process(_delta):
-	if Input.is_action_just_pressed("spacebar"):
-		get_tree().change_scene("res://scenes/World.tscn")
-
 func _on_button_pressed():
 	get_tree().change_scene("res://scenes/World.tscn")
